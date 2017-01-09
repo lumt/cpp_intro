5ba6fe8ca041081fe2e69a40387582c925059b4a
LT <apple@MacBook-Pro-R.home>
2017-01-09 00:46:18 +0000

    shrink test

--- exam/shrink/shrink.cpp
+++ exam/shrink/shrink.cpp
@@ -43,6 +43,7 @@ void encode(const char* word, char* compressed, const char** dictionary) {
       // val[0] = result % 10 + '0';
       // val[1] = (result - result % 10)/10 + '0';
       itoa(result, val, 10);
+      // testing itoa
     }
 
     val[2] = '\0';
